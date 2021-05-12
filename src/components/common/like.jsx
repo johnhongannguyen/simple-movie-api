985a47927c9a4dc2caa219b8dbfa71e458d169f4
johnhongannguyen <johhaneshongan@gmail.com>
2021-05-12 08:00:40 -0700

    add like feature

---
+++ src/components/common/like.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// Input: liked: boolean
+// Output: onClick
+const Like = (props) => {
+    let classes = "fa fa-heart";
+    if(!props.liked) classes += "-o";
+    return ( 
+    <i 
+      onClick={props.onClick}
+      className={classes} 
+      aria-hidden="true"
+      style={{cursor:"pointer"}} 
+      /> 
+      
+      );
+    
+};
+ export default Like;
