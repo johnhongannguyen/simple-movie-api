0d1c87eb5e8f8c6c4a069253251fe2409e6cc920
johnhongannguyen <johhaneshongan@gmail.com>
2021-05-17 23:52:58 -0700

    displaying items

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -44,10 +44,12 @@ class Movies extends Component {
         const movies = paginate(allMovies, currentPage, pageSize);
         return (  
             <div className="row">
-                <div className="col-2">
+                <div className="col-3">
                     <ListGroup 
                     items={this.state.genres}
                     onItemSelect={this.handleGenreSelect}
+                    textProperty="name"
+                    valueProperty="_id"
                     />
                 </div>
                 <div className="col">
