7efd69b8d8bad16e64b923dcc27b1dca30a9dd4e
johnhongannguyen <johhaneshongan@gmail.com>
2021-05-21 14:49:27 -0700

    moving responsibility

--- src/components/movies.js
+++ src/components/movies.js
@@ -11,7 +11,7 @@ class Movies extends Component {
     state = {  
         movies: [],
         genres: [],
-        pageSize:5,
+        pageSize:4,
         currentPage: 1,
         sortColumn: {path: 'title', order: 'asc'}
     }
@@ -40,14 +40,8 @@ class Movies extends Component {
         movies[index].liked= !movies[index].liked;
         this.setState({movies});
     }
-    handleSort = (path) =>{
-        const sortColumn = {...this.state.sortColumn};
-        if(sortColumn.path === path)
-            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
-        else{
-            sortColumn.path = path;
-            sortColumn.order = "asc";
-        }
+    handleSort = (sortColumn) =>{
+       
         this.setState({sortColumn});
     }
     render() { 
@@ -71,6 +65,7 @@ class Movies extends Component {
                 <p>Showing {filtered.length} movies in the database.</p>
                 <MoviesTable 
                 movies={movies} 
+                sortColumn={sortColumn}
                 onLike={this.handleLike} 
                 onDelete={this.handleDelete}
                 onSort={this.handleSort}
