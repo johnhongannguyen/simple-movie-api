9afdc081082fd57f5f8dc5ed1a26f4be837d933c
johnhongannguyen <johhaneshongan@gmail.com>
2021-05-25 07:39:44 -0700

    extracting table

---
+++ src/components/common/table.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TableHeader from './tableHeader'
+import TableBody from './tableBody';
+
+const Table = (props) => {
+    const {columns, sortColumn, onSort, data} = props;
+    return ( 
+        <table className="table">
+        <TableHeader 
+        columns ={this.columns}
+        sortColumn ={sortColumn}
+        onSort ={onSort}
+        />
+        <TableBody 
+         columns={this.columns}
+         data={data}
+        />
+         
+     </table>
+     );
+}
+ 
+export default Table;
--- src/components/moviesTable.js
+++ src/components/moviesTable.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
+import Table from './common/table'
 import Like from './common/like';
-import TableHeader from './common/tableHeader'
-import TableBody from './common/tableBody';
+
 
 
 
@@ -28,18 +28,13 @@ class MoviesTable extends Component {
         const {movies, onSort, sortColumn} = this.props;
 
     return ( 
-        <table className="table">
-               <TableHeader 
-               columns ={this.columns}
-               sortColumn ={sortColumn}
-               onSort ={onSort}
-               />
-               <TableBody 
-                columns={this.columns}
-                data={movies}
-               />
-                
-            </table>
+       <Table 
+       columns ={this.columns}
+       data = {movies}
+       sortColumn ={sortColumn}
+       onSort={onSort}
+
+       />
      );
     }
 }
